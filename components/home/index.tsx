b5abee228c8d07b6e241eeef111e8fa9e34bfb24
acomarcho <acoxstpd@gmail.com>
2023-08-01 14:59:24 +0700

    Feat: Add second home section

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -33,7 +33,51 @@ export default function Home() {
             </Link>
           </div>
         </div>
-        <Image src="/images/hero_code.png" alt="" width={481} height={652} className="mx-auto" />
+        <Image
+          src="/images/hero_code.png"
+          alt=""
+          width={481}
+          height={652}
+          className="mx-auto"
+        />
+      </div>
+      <div className="flex flex-col gap-[1rem] lg:flex-row-reverse lg:items-center mt-[2rem]">
+        <div>
+          <h1 className="heading text-white">
+            Materi yang <span className="text-cyan">terstruktur</span>.{" "}
+            <span className="text-red">Tidak bertele-tele</span> dengan
+            penjelasan yang tidak penting.{" "}
+            <span className="text-yellow italic">
+              We value your time, attention, and energy
+            </span>
+            .
+          </h1>
+          <p className="paragraph text-lightgray mt-[1rem]">
+            Terinspirasi dari kelas-kelas perkuliahan yang lama dan membosankan,
+            kami berusaha menyediakan intisari dari ilmu pemrograman yang kami
+            dapatkan di ITB secara ringkas tanpa memotong pemahaman. Kelas-kelas
+            yang kami bawa{" "}
+            <span className="font-bold text-white">
+              berorientasi problem solving: terfokus pada latihan-latihan
+              praktikal
+            </span>
+            .
+          </p>
+          <p className="paragraph text-lightgray mt-[1rem]">
+            Materi kami sajikan dalam bentuk{" "}
+            <span className="font-bold text-white">teks</span>,{" "}
+            <span className="font-bold text-white">video</span>, dan{" "}
+            <span className="font-bold text-white">soal-soal latihan</span>{" "}
+            untuk Anda melakukan uji pemahaman.
+          </p>
+        </div>
+        <Image
+          src="/images/hero_read.png"
+          alt=""
+          width={316}
+          height={392}
+          className="mx-auto"
+        />
       </div>
     </div>
   );
