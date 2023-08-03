6ae7842cf996279e5331cf3ec90ebfe0489b772b
acomarcho <acoxstpd@gmail.com>
2023-08-03 22:42:57 +0700

    Feat: Add redis for module chunks

--- pages/api/chunk/[id]/index.tsx
+++ pages/api/chunk/[id]/index.tsx
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import type {
+  Chunk,
   GetSingleChunkResponse,
   ErrorResponse,
 } from "@/lib/constants/responses";
@@ -7,6 +8,8 @@ import { prisma } from "@/lib/db";
 import { checkAuth } from "@/lib/utils";
 import fs from "fs/promises";
 import path from "path";
+import { RedisConnection } from "@/lib/db/redis";
+import { DEFAULT_EXPIRATION } from "@/lib/constants/redis";
 
 export default async function handler(
   req: NextApiRequest,
@@ -30,22 +33,42 @@ export default async function handler(
       return res.status(401).json({ message: "Identitas Anda salah." });
     }
 
-    const chunk = await prisma.chunk.findUnique({
-      where: {
-        id: parseInt(id),
-      },
-    });
+    const redisClient = await RedisConnection.getInstance();
+    const redisKey = `chunk:${id}`;
+    const cachedData = await redisClient.get(redisKey);
+    let chunk: Chunk | null;
 
-    if (!chunk) {
-      return res.status(200).json({ chunk: null });
+    if (!cachedData) {
+      chunk = await prisma.chunk.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+      });
+
+      if (!chunk) {
+        await redisClient.setEx(
+          redisKey,
+          DEFAULT_EXPIRATION,
+          JSON.stringify(chunk)
+        );
+        return res.status(200).json({ chunk: null });
+      }
+
+      const fileContent = await fs.readFile(
+        path.resolve("./public", "content", chunk.content),
+        "utf-8"
+      );
+
+      chunk.content = fileContent;
+
+      await redisClient.setEx(
+        redisKey,
+        DEFAULT_EXPIRATION,
+        JSON.stringify(chunk)
+      );
+    } else {
+      chunk = JSON.parse(cachedData);
     }
-
-    const fileContent = await fs.readFile(
-      path.resolve("./public", "content", chunk.content),
-      "utf-8"
-    );
-
-    chunk.content = fileContent;
 
     return res.status(200).json({ chunk });
   } catch (error) {
