7ff57d71e1bea82363ce84e75c86e9f58756baf4
acomarcho <acoxstpd@gmail.com>
2023-08-05 10:27:46 +0700

    Feat: Add redis to user API

--- lib/utils/api.ts
+++ lib/utils/api.ts
@@ -1,6 +1,8 @@
 import { prisma } from "../db";
 import jwt from "jwt-simple";
 import type { User } from "../constants/responses";
+import { RedisConnection } from "@/lib/db/redis";
+import { DEFAULT_EXPIRATION } from "@/lib/constants/redis";
 
 export const extractToken = (authHeader: string) => {
   return authHeader.split(" ")[1];
@@ -16,14 +18,26 @@ export const checkAuth = async (authHeader: string) => {
     return null;
   }
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email: decodedJwt!.email,
-    },
-  });
+  const redisClient = await RedisConnection.getInstance();
+  const redisKey = `user:${decodedJwt.email}`;
+  const cachedData = await redisClient.get(redisKey);
+
+  let user: User | null;
+
+  if (!cachedData) {
+    user = await prisma.user.findUnique({
+      where: {
+        email: decodedJwt!.email,
+      },
+    });
+    await redisClient.setEx(redisKey, DEFAULT_EXPIRATION, JSON.stringify(user));
+  } else {
+    user = JSON.parse(cachedData);
+  }
+
   if (!user) {
     return null;
   }
 
-  return decodedJwt;
+  return user;
 };
