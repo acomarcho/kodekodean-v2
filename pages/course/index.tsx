8c9fb38d1e6653d6ad59e5a2f4a9e2e33b227ecd
acomarcho <acoxstpd@gmail.com>
2023-08-05 11:28:53 +0700

    Feat: Add title and meta tag at course

--- pages/course/index.tsx
+++ pages/course/index.tsx
@@ -1,9 +1,17 @@
 import Course from "@/components/course";
 import Navbar from "@/components/navbar";
+import Head from "next/head";
 
 export default function CoursesPage() {
   return (
     <>
+      <Head>
+        <title>Courses</title>
+        <meta
+          name="description"
+          content="Pilih salah satu kursus yang kami sediakan dalam kodekodean.id!"
+        />
+      </Head>
       <Navbar />
       <Course />
     </>
